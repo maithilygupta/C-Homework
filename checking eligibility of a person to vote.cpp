e414e99e50194c713e8926560ac4d120e4a8dc88
maithilygupta <69429258+maithilygupta@users.noreply.github.com>
2020-08-19 17:37:39 +0530

    Add files via upload

---
+++ checking eligibility of a person to vote.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{	int i;
+cout<<"Enter your age = ";
+cin>>i;
+if(i>=18){
+	cout<<"User is eligible to vote"<<"\n";
+}
+else{
+	cout<<"User is not eligible to vote";
+}
+return 0;
+}
